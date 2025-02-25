62b4370cbc84d179069227241ce77ead0da683f7
Adham <adhammostfa310@gmail.com>
2025-02-26 02:40:10 +0300

    chore: extract messages

--- app/locales/ar/messages.js
+++ app/locales/ar/messages.js
@@ -1,3 +1,5 @@
 /*eslint-disable*/ module.exports = {
-  messages: JSON.parse('{"uzTaYi":["نعم انه انا هههههه"]}'),
+  messages: JSON.parse(
+    '{"fPNjA+":["© ",["0"]," Reactham. MIT License."],"lfXRfk":["A meticulously crafted template for building production-grade applications with TypeScript, React Router, and modern tooling."],"CWe7wB":["Changelog"],"cpUcSj":["Clean separation of client/server concerns with SSR support"],"HH5U/J":["Enterprise Ready Foundation"],"GiP6XD":["Full TypeScript integration with strict checks"],"c3b0B0":["Get Started"],"A8sO4i":["Git Ready"],"RkXlPZ":["GitHub"],"uzTaYi":["مرحبا"],"QqS3p+":["Modular Architecture"],"wbUjpH":["Optimized builds with Tree-shaking & Code splitting"],"HL+pi3":["Pre-configured hooks and commit conventions"],"LhfA3X":["Production Grade"],"+ZxV3Q":["Type Safe"],"hy6L14":["View on GitHub"]}',
+  ),
 };
